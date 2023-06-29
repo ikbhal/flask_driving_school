776bd9d29e1a2ef270c420bbf15a56bb3bf2e071
Shaik Ikbhal Basha <iqbalforall@gmail.com>
2023-06-29 19:28:49 +0530

    fix missing self parameter in student_manager

--- managers/student_manager.py
+++ managers/student_manager.py
@@ -5,10 +5,10 @@ class StudentManager:
     def __init__(self, mongo_client):
         self.mongo_client = mongo_client
 
-    def get_student_by_id(id):
+    def get_student_by_id(self, id):
         student = self.mongo_client.db.students.find_one({'_id': ObjectId(id)})
         return student
     
-    def get_student_by_mobile_number(mobile_number):
+    def get_student_by_mobile_number(self, mobile_number):
         student = self.mongo_client.db.students.find_one({'mobile_number': mobile_number})
         return student
