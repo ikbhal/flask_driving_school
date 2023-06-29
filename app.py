30b8d362ac39d2670b874f00ce3717291ece5c8d
Shaik Ikbhal Basha <iqbalforall@gmail.com>
2023-06-29 07:05:29 +0530

    adding fields joining_date, course, amount, discount, amount_paid, settle_amount

--- app.py
+++ app.py
@@ -1,98 +1,82 @@
-# create flask object
-# run flask object
-# import flask object
-# use mongodb to store data
-# improt mongodb relavent module python
 from flask import Flask, render_template, request, redirect, url_for, session, flash
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
-# import uuid module
-# import uuid
 
-# create Student class with properties id ,name, mobile number
 class Student:
-    def __init__(self,  name, mobile_numer):
-        # self.id = id
+    def __init__(self, name, mobile_number, joining_date, application_number, amount, discount, amount_paid, settle_amount, course):
         self.name = name
-        self.mobile_numer = mobile_numer
+        self.mobile_number = mobile_number
+        self.joining_date = joining_date
+        self.application_number = application_number
+        self.amount = amount
+        self.discount = discount
+        self.amount_paid = amount_paid
+        self.settle_amount = settle_amount
+        self.course = course
 
 app = Flask(__name__)
 app.secret_key = 'allah raji hojaye'
-
-# create pymongo object, specify mongodb uri localhost:27017 skool database
-# mongo_client = PyMongo(app)
 mongo_client = PyMongo(app, uri="mongodb://localhost:27017/skool")
-
-# access mongo db from client
-# access student collection from mongo db
 student_collection = mongo_client.db.students
 
 @app.route('/')
 def index():
     student_list = student_collection.find()
-    print('student_list: ' , student_list)
-    # for student in student_list:
-    #     print('student: ' , student)
-
     return render_template('index.html', student_list=student_list)
 
-# create route to display list of students
 @app.route('/list_students')
 def list_students():
-    print("inside list students route");
     student_list = student_collection.find()
     return render_template('list_students.html', student_list=student_list)
 
-
-# create add student route
 @app.route('/add_student', methods=['POST', 'GET'])
 def add_student():
     if request.method == 'POST':
         name = request.form['name']
-        mobile_numer = request.form['mobile_number']
-        student = Student(name, mobile_numer)
-        print(student.name)
-        print(student.mobile_numer)
-        # save student to mongo db
-        student_collection.insert_one(
-            {
-                'name': student.name,
-                'mobile_number': student.mobile_numer
-            })
+        mobile_number = request.form['mobile_number']
+        joining_date = request.form['joining_date']
+        application_number = request.form['application_number']
+        amount = request.form['amount']
+        discount = request.form['discount']
+        amount_paid = request.form['amount_paid']
+        settle_amount = request.form['settle_amount']
+        course = request.form['course']
 
-        # add flash messaage on success
+        student = Student(name, mobile_number, joining_date, application_number, amount, discount, amount_paid, settle_amount, course)
+        
+        student_collection.insert_one({
+            'name': student.name,
+            'mobile_number': student.mobile_number,
+            'joining_date': student.joining_date,
+            'application_number': student.application_number,
+            'amount': student.amount,
+            'discount': student.discount,
+            'amount_paid': student.amount_paid,
+            'settle_amount': student.settle_amount,
+            'course': student.course
+        })
+
         flash('Student Added Successfully')
         return redirect(url_for('index'))
     else:
-        # sent add student form  html page
         return render_template('add_student.html')
 
-# create delete student by id route 
 @app.route('/delete_student/<student_id>')
 def delete_student(student_id):
-    # delete student by id
-    print('delete student _id: ' , student_id)
     student_collection.delete_one({'_id': ObjectId(student_id)})
-    # add flash message
     flash('Student Deleted Successfully')
     return redirect(url_for('index'))
 
-# search student by mobile number route
 @app.route('/search_student', methods=['POST', 'GET'])
 def search_student():
     if request.method == 'POST':
         mobile_number = request.form['mobile_number']
-        print('mobile_number: ' , mobile_number)
-        # search student by mobile number
         student = student_collection.find_one({'mobile_number': mobile_number})
-        print('student: ' , student)
         if student:
-            # if student found
             return render_template('search_student.html', student=student)
-        
-    else:
-        # return redirect(url_for('index'))
-        return render_template('search_student.html');
+        else:
+            flash('Student not found')
+    return render_template('search_student.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
